3bd9bb0e0d57b95966726d50d3f069ef798e958f
GMODReality <90872353+GMODReality@users.noreply.github.com>
2021-09-20 15:17:10 +0100

    Delete st directory

--- st/lua/autorun/st_addon.lua
+++
@@ -1,4 +0,0 @@
-player_manager.AddValidModel( "Stormtrooper", "models/player/swe/st.mdl" );
-player_manager.AddValidHands( "Stormtrooper", "models/weapons/c_arms_st.mdl", 0, "00000000" );
-list.Set( "PlayerOptionsModel",  "Stormtrooper", "models/player/swe/st.mdl" );
-sound.AddSoundOverrides("scripts/vehicles/npc_sounds_st.txt")
